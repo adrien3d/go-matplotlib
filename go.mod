5ac4568f4bfe9100263e2672c49dda208a80b812
Adrien Chapelet <adrien3d@gmail.com>
2020-06-25 00:06:37 +0200

    Using fyne

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/adrien3d/go-matplotlib
 go 1.14
 
 require (
+	fyne.io/fyne v1.3.0
 	github.com/andlabs/ui v0.0.0-20200610043537-70a69d6ae31e
+	github.com/fyne-io/examples v0.0.0-20200516201522-4bcb94be4ee2
 	github.com/gotk3/gotk3 v0.4.0 // indirect
 	github.com/mattn/go-gtk v0.0.0-20191030024613-af2e013261f5 // indirect
 	github.com/mattn/go-pointer v0.0.0-20190911064623-a0a44394634f // indirect
--- main.go
+++ main.go
@@ -2,232 +2,100 @@ package main
 
 import (
 	"fmt"
-	"github.com/andlabs/ui"
-	_ "github.com/andlabs/ui/winmanifest"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/driver/desktop"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"fyne.io/fyne"
+	"fyne.io/fyne/canvas"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/widget"
+	"github.com/fyne-io/examples/img/icon"
 )
 
 var (
-	histogram   *ui.Area
-	resetButton *ui.Button
-	positionLabel *ui.Label
-
-	currentPoint = -1
+	pl Plotlib
 )
 
-// some metrics
-const (
-	xoffLeft    = 20 // histogram margins
-	yoffTop     = 20
-	xoffRight   = 20
-	yoffBottom  = 20
-	pointRadius = 5
-)
-
-// helper to quickly set a brush color
-func mkSolidBrush(color uint32, alpha float64) *ui.DrawBrush {
-	brush := new(ui.DrawBrush)
-	brush.Type = ui.DrawBrushTypeSolid
-	component := uint8((color >> 16) & 0xFF)
-	brush.R = float64(component) / 255
-	component = uint8((color >> 8) & 0xFF)
-	brush.G = float64(component) / 255
-	component = uint8(color & 0xFF)
-	brush.B = float64(component) / 255
-	brush.A = alpha
-	return brush
+// Plotlib is an app to get Plotlib graph and display it
+type Plotlib struct {
+	image *canvas.Image
+	coord *widget.Label
 }
 
-// and some colors
-// names and values from https://msdn.microsoft.com/en-us/library/windows/desktop/dd370907%28v=vs.85%29.aspx
-const (
-	colorWhite      = 0xFFFFFF
-	colorBlack      = 0x000000
-	colorDodgerBlue = 0x1E90FF
-)
-
-func pointLocations(width, height float64) (xs, ys [10]float64) {
-	xincr := width / 9 // 10 - 1 to make the last point be at the end
-	yincr := height / 100
-	for i := 0; i < 10; i++ {
-		// get the value of the point
-		n := 50
-		// because y=0 is the top but n=0 is the bottom, we need to flip
-		n = 100 - n
-		xs[i] = xincr * float64(i)
-		ys[i] = yincr * float64(n)
+func (pl *Plotlib) downloadImage(url string) {
+	response, e := http.Get(url)
+	if e != nil {
+		log.Fatal(e)
 	}
-	return xs, ys
-}
+	defer response.Body.Close()
 
-func constructGraph(width, height float64, extend bool) *ui.DrawPath {
-	xs, ys := pointLocations(width, height)
-	path := ui.DrawNewPath(ui.DrawFillModeWinding)
+	file, err := ioutil.TempFile(os.TempDir(), "Plotlib.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-	path.NewFigure(xs[0], ys[0])
-	for i := 1; i < 10; i++ {
-		path.LineTo(xs[i], ys[i])
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	if extend {
-		path.LineTo(width, height)
-		path.LineTo(0, height)
-		path.CloseFigure()
-	}
-
-	path.End()
-	return path
+	pl.image.File = file.Name()
+	canvas.Refresh(pl.image)
 }
 
-func graphSize(clientWidth, clientHeight float64) (graphWidth, graphHeight float64) {
-	return clientWidth - xoffLeft - xoffRight, clientHeight - yoffTop - yoffBottom
+type CustomImage struct {
+	widget.Box
 }
 
-type areaHandler struct{}
-
-func (areaHandler) Draw(a *ui.Area, p *ui.AreaDrawParams) {
-	// fill the area with white
-	brush := mkSolidBrush(colorWhite, 1.0)
-	path := ui.DrawNewPath(ui.DrawFillModeWinding)
-	path.AddRectangle(0, 0, p.AreaWidth, p.AreaHeight)
-	path.End()
-	p.Context.Fill(path, brush)
-	path.Free()
-
-	graphWidth, graphHeight := graphSize(p.AreaWidth, p.AreaHeight)
-
-	sp := &ui.DrawStrokeParams{
-		Cap:        ui.DrawLineCapFlat,
-		Join:       ui.DrawLineJoinMiter,
-		Thickness:  2,
-		MiterLimit: ui.DrawDefaultMiterLimit,
-	}
-
-	// draw the axes
-	brush = mkSolidBrush(colorBlack, 1.0)
-	path = ui.DrawNewPath(ui.DrawFillModeWinding)
-	path.NewFigure(xoffLeft, yoffTop)
-	path.LineTo(xoffLeft, yoffTop+graphHeight)
-	path.LineTo(xoffLeft+graphWidth, yoffTop+graphHeight)
-	path.End()
-	p.Context.Stroke(path, brush, sp)
-	path.Free()
-
-	// now transform the coordinate space so (0, 0) is the top-left corner of the graph
-	m := ui.DrawNewMatrix()
-	m.Translate(xoffLeft, yoffTop)
-	p.Context.Transform(m)
-
-	// now get the color for the graph itself and set up the brush
-	//graphR, graphG, graphB, graphA := colorButton.Color()
-	brush.Type = ui.DrawBrushTypeSolid
-	brush.R = 0
-	brush.G = 0.2
-	brush.B = 1
-	// we set brush.A below to different values for the fill and stroke
-
-	// now create the fill for the graph below the graph line
-	path = constructGraph(graphWidth, graphHeight, true)
-	brush.A = 100 / 2
-	p.Context.Fill(path, brush)
-	path.Free()
-
-	// now draw the histogram line
-	path = constructGraph(graphWidth, graphHeight, false)
-	brush.A = 100
-	p.Context.Stroke(path, brush, sp)
-	path.Free()
-
-	// now draw the point being hovered over
-	if currentPoint != -1 {
-		xs, ys := pointLocations(graphWidth, graphHeight)
-		path = ui.DrawNewPath(ui.DrawFillModeWinding)
-		path.NewFigureWithArc(
-			xs[currentPoint], ys[currentPoint],
-			pointRadius,
-			0, 6.23, // TODO pi
-			false)
-		path.End()
-		// use the same brush as for the histogram lines
-		p.Context.Fill(path, brush)
-		path.Free()
-	}
+func (img *CustomImage) MouseIn(*desktop.MouseEvent) {
+	fmt.Println("Entered")
 }
 
-func inPoint(x, y float64, xtest, ytest float64) bool {
-	// TODO switch to using a matrix
-	x -= xoffLeft
-	y -= yoffTop
-	return (x >= xtest-pointRadius) &&
-		(x <= xtest+pointRadius) &&
-		(y >= ytest-pointRadius) &&
-		(y <= ytest+pointRadius)
+func (img *CustomImage) MouseOut() {
+	fmt.Println("Exited")
 }
 
-func (areaHandler) MouseEvent(a *ui.Area, me *ui.AreaMouseEvent) {
-	graphWidth, graphHeight := graphSize(me.AreaWidth, me.AreaHeight)
-	xs, ys := pointLocations(graphWidth, graphHeight)
-
-	currentPoint = -1
-	for i := 0; i < 10; i++ {
-		if inPoint(me.X, me.Y, xs[i], ys[i]) {
-			currentPoint = i
-			break
-		}
-	}
-	//fmt.Println(me.X, me.Y)
-	positionLabel.SetText("X:" + fmt.Sprintf("%f",me.X) + "\t Y:"+ fmt.Sprintf("%f",me.Y))
-
-	// TODO only redraw the relevant area
-	histogram.QueueRedrawAll()
+func (img *CustomImage) MouseMoved(me *desktop.MouseEvent) {
+	pl.coord.SetText("Coord: X:" + strconv.Itoa(me.Position.X) + "    Y:" + strconv.Itoa(me.Position.Y))
 }
 
-func (areaHandler) MouseCrossed(a *ui.Area, left bool) {
-	// do nothing
-}
+// Show starts a new Plotlib widget
+func Show(app fyne.App) {
+	w := app.NewWindow("Plotlib Viewer")
+	w.SetIcon(icon.XKCDBitmap)
 
-func (areaHandler) DragBroken(a *ui.Area) {
-	// do nothing
-}
-
-func (areaHandler) KeyEvent(a *ui.Area, ke *ui.AreaKeyEvent) (handled bool) {
-	// reject all keys
-	return false
-}
-
-func setupUI() {
-	mainwin := ui.NewWindow("Figure 1", 640, 480, true)
-	mainwin.SetMargined(false)
-	mainwin.OnClosing(func(*ui.Window) bool {
-		mainwin.Destroy()
-		ui.Quit()
-		return false
+	go pl.downloadImage("https://www.google.fr/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png")
+	submit := widget.NewButton("Submit", func() {
+		//x.Submit()
 	})
-	ui.OnShouldQuit(func() bool {
-		mainwin.Destroy()
-		return true
-	})
-
-	vbox := ui.NewVerticalBox()
-	vbox.SetPadded(true)
-	mainwin.SetChild(vbox)
-	//msggrid := ui.NewGrid()
-
-	histogram = ui.NewArea(areaHandler{})
-	resetButton = ui.NewButton("Home")
-
-	vbox.Append(histogram, false)
-	vbox.Append(ui.NewVerticalSeparator(), false)
-
-	hbox := ui.NewHorizontalBox()
-	hbox.SetPadded(true)
-	hbox.Append(resetButton, false)
-	positionLabel = ui.NewLabel("X: \t Y:")
-	hbox.Append(positionLabel, false)
-	vbox.Append(hbox, false)
-
-	mainwin.Show()
+	submit.Style = widget.PrimaryButton
+	pl.coord = widget.NewLabel("Coordinates")
+	pl.coord.Alignment = fyne.TextAlignCenter
+	buttonsBox := widget.NewHBox(
+		widget.NewButton("Exit", func() {
+			w.Close()
+		}),
+		layout.NewSpacer(),
+		pl.coord,
+		submit)
+	pl.image = &canvas.Image{FillMode: canvas.ImageFillOriginal}
+	imageBox := widget.NewHBox(pl.image)
+	content := &CustomImage{*imageBox}
+	vbox := widget.NewVBox(content, buttonsBox)
+	w.SetContent(fyne.NewContainerWithLayout(layout.NewBorderLayout(nil, nil, nil, nil), vbox))
+	w.Show()
 }
 
 func main() {
-	ui.Main(setupUI)
+	launch := Show
+	ex := app.New()
+	launch(ex)
+	ex.Run()
 }
